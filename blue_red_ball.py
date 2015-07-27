a2290b371608f4569d8ac4fac1a69c3a645fff81
chenziheziye <chenziheziye@gmail.com>
2015-07-28 00:35:45 +0800

    first time trying to write code caculating hot red num.

--- blue_red_ball.py
+++ blue_red_ball.py
@@ -1,9 +1,20 @@
 import re
+import numpy as np
 
-path = '/Users/chenziheziye/Documents/Study_inbox/study_in_BJ/s.txt'
+path = '/Users/chenziheziye/PycharmProjects/test/ssq.txt'
+hist_array = ['00']*10
+
 with open(path, 'rt') as f:
-	for line in f:
-		raw_val = re.split(r'[,\s\\]', line)
-		print raw_val
-		
+    for line in f:
+        raw_val = re.split(r'[,\s\\]', line.rstrip())
+
+        if len(raw_val) > 10:
+            raw_val.pop()
+
+        print raw_val
+        nd_array = np.vstack((hist_array, raw_val))
+        hist_array = nd_array
+
+        #each line refers to a priority list for red and blue num
+
 f.close()
